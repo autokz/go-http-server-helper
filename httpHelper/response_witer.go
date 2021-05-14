300b90cdf09c241189f61ebeba960fb7abb0707a
tyanrv <roman-t@auto.kz>
2021-05-14 16:01:13 +0600

    fix area visibility

--- httpHelper/response_witer.go
+++ httpHelper/response_witer.go
@@ -7,26 +7,26 @@ import (
 )
 
 type ResponseWriterLazy struct {
-	w    http.ResponseWriter
-	buf  bytes.Buffer
-	code int
+	Writer http.ResponseWriter
+	Buffer bytes.Buffer
+	Code   int
 }
 
 func (rw *ResponseWriterLazy) Header() http.Header {
-	return rw.w.Header()
+	return rw.Writer.Header()
 }
 
 func (rw *ResponseWriterLazy) WriteHeader(statusCode int) {
-	rw.code = statusCode
+	rw.Code = statusCode
 }
 
 func (rw *ResponseWriterLazy) Write(data []byte) (int, error) {
-	return rw.buf.Write(data)
+	return rw.Buffer.Write(data)
 }
 
 func (rw *ResponseWriterLazy) Done() (int64, error) {
-	if rw.code > 0 {
-		rw.w.WriteHeader(rw.code)
+	if rw.Code > 0 {
+		rw.Writer.WriteHeader(rw.Code)
 	}
-	return io.Copy(rw.w, &rw.buf)
+	return io.Copy(rw.Writer, &rw.Buffer)
 }
