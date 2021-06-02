29bd2503535f5e658d6ee0d85d7ef3127c06c776
tyanrv <roman-t@auto.kz>
2021-06-02 16:33:08 +0600

    v1.0.4: Fix: check for request method OPTIONS

--- httpHelper/method_const.go
+++ httpHelper/method_const.go
@@ -1,8 +1,9 @@
 package httpHelper
 
 const (
-	GET_METHOD    = "GET"
-	POST_METHOD   = "POST"
-	PUT_METHOD    = "PUT"
-	DELETE_METHOD = "DELETE"
+	GET_METHOD     = "GET"
+	POST_METHOD    = "POST"
+	PUT_METHOD     = "PUT"
+	DELETE_METHOD  = "DELETE"
+	OPTIONS_METHOD = "OPTIONS"
 )
