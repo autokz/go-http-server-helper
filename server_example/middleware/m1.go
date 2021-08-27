12e1af01f2ffb9d3910be1bf914577dac2987193
tyanrv <roman-t@auto.kz>
2021-08-27 18:12:49 +0600

    Dev: add server client example

---
+++ server_example/middleware/m1.go
@@ -0,0 +1,38 @@
+package middleware
+
+import (
+	"log"
+	"net/http"
+)
+
+func M1(next http.HandlerFunc) http.HandlerFunc {
+	return func(writer http.ResponseWriter, request *http.Request) {
+		log.Println(1)
+		writer.Header().Set("M", "1")
+		next(writer, request)
+	}
+}
+
+func M2(next http.HandlerFunc) http.HandlerFunc {
+	return func(writer http.ResponseWriter, request *http.Request) {
+		log.Println(2)
+		writer.Header().Set("M2", "2")
+		next(writer, request)
+	}
+}
+
+func MainMiddleware1(next http.HandlerFunc) http.HandlerFunc {
+	return func(writer http.ResponseWriter, request *http.Request) {
+		log.Println("MainMiddleware1")
+		writer.Header().Set("Main", "MAIN")
+		next(writer, request)
+	}
+}
+
+func MainMiddleware2(next http.HandlerFunc) http.HandlerFunc {
+	return func(writer http.ResponseWriter, request *http.Request) {
+		log.Println("MainMiddleware2")
+		writer.Header().Set("Main2", "MAIN")
+		next(writer, request)
+	}
+}
---
+++ server_example/server.go
@@ -0,0 +1,38 @@
+package server
+
+import (
+	"github.com/autokz/go-http-server-helper/httpHelper"
+	"github.com/autokz/go-http-server-helper/server_example/routes"
+	"log"
+	"net/http"
+	"time"
+)
+
+func getRoutes() *http.ServeMux {
+	mux := http.NewServeMux()
+
+	httpHelper.RegisterGroupRoutes(
+		mux,
+		routes.TestRoutes,
+		routes.TestRoutes2,
+	)
+
+	httpHelper.RegisterRoute(mux, routes.Route4)
+
+	return mux
+}
+
+func InitServer() {
+	server := &http.Server{
+		Addr:         ":8099",
+		Handler:      getRoutes(),
+		ReadTimeout:  30 * time.Second,
+		WriteTimeout: 30 * time.Second,
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		log.Fatalln(err)
+		return
+	}
+}
